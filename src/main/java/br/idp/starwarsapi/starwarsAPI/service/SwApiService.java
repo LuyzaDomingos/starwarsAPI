d3e640263ca32f72c9cd5b0aa66566f40cc0d866
LuyzaDomingos <luyzadomingos@cc.ci.ufpb.br>
2021-10-25 00:24:23 -0300

    Mudanças...
    Método no Service, Cache e Exceção - application.properties

--- src/main/java/br/idp/starwarsapi/starwarsAPI/service/SwApiService.java
+++ src/main/java/br/idp/starwarsapi/starwarsAPI/service/SwApiService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.h2.value.ValueJson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +12,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -18,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import br.idp.starwarsapi.starwarsAPI.config.validation.ErrorForm;
 import br.idp.starwarsapi.starwarsAPI.controller.PlanetController;
 import br.idp.starwarsapi.starwarsAPI.exception.ConnectionException;
 import br.idp.starwarsapi.starwarsAPI.exception.PlanetNotFoundException;
@@ -51,14 +54,6 @@ public class SwApiService {
 	private String headerName;
 	
 	
-
-	/**
-	 * Get planets from swapi from the id
-	 * @param id
-	 * @return
-	 * @throws ConnectionException
-	 * @throws PlanetNotFoundException if planet dont exist
-	 */
 	@Cacheable(value = "getSwapiPlanetsId")
 	public SwApiPlanet getSwapiPlanetsId(Long id) throws ConnectionException, PlanetNotFoundException {
 		log.info("Acessando api para buscar planetas pelo id...");
@@ -73,35 +68,27 @@ public class SwApiService {
 			return restTemplate.getForObject(swUrl, SwApiPlanet.class);
 
 		}
-		throw new PlanetNotFoundException("Planet not exist in the API");
+		throw new PlanetNotFoundException("Planet dont exist in the API!!");
 
 	}
 	
 	
 	public int countFilmsByPlanet(String planetName) throws IOException {
-		if (!checkConnection())
-			throw new ConnectionException("Not connection detection");
-
-		
-		headers.add(headerName,headerValue);
-		HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-
-		ResponseEntity<String> response = restTemplate.exchange(sw_url_name + planetName,
-				HttpMethod.GET, entity, String.class);
-
-		JsonNode root = new ObjectMapper().readTree(response.getBody());
-
-		if (root != null && root.get("count").asLong() > 0) {
-			JsonNode results = root.get("results");
-			JsonNode planet = results.get(0);
-			JsonNode films = planet.get("films");
-			return films.size();
-		}
-
-		return 0;
+		JsonNode planetJsonNode;
+		planetJsonNode = counting(planetName);
+		JsonNode  films = planetJsonNode.get("films");
+		return films.size();
 	}
 	
 	public int countResidentsByPlanet(String planetName) throws IOException {
+		JsonNode planetJsonNode;
+		planetJsonNode = counting(planetName);
+		JsonNode residents = planetJsonNode.get("residents");
+		return residents.size();
+	}
+	
+	
+	public JsonNode counting(String planetName) throws IOException {
 		if (!checkConnection())
 			throw new ConnectionException("Not connection detection");
 
@@ -117,14 +104,14 @@ public class SwApiService {
 		if (root != null && root.get("count").asLong() > 0) {
 			JsonNode results = root.get("results");
 			JsonNode planet = results.get(0);
-			JsonNode residents = planet.get("residents");
-			return residents.size();
+			return planet;
 		}
+		
+		throw new PlanetNotFoundException("Planet dont exist in SW API.....");
 
-		return 0;
 	}
 	
-
+	
 	private boolean checkConnection() {
 		try {
 			URL url = new URL(sw_base);
