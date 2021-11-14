47c76295ce7b64f7a7cdf826af1cad56e1cee809
LuyzaDomingos <luyzadomingos@cc.ci.ufpb.br>
2021-11-13 23:22:16 -0300

    Testes Funcionais

---
+++ src/test/java/br/idp/starwarsapi/starwarsAPI/functional/PlanetControllerTest.java
@@ -0,0 +1,95 @@
+package br.idp.starwarsapi.starwarsAPI.functional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.URI;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.idp.starwarsapi.starwarsAPI.model.Planet;
+import br.idp.starwarsapi.starwarsAPI.service.SwApiService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class PlanetControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	private Planet mockPlanet;
+	
+	@MockBean
+	private SwApiService swApiService;
+	
+	@BeforeEach
+	public void setUp() {
+		mockPlanet = new Planet();
+		mockPlanet.setName("Sullust");
+		mockPlanet.setClimate("superheated");
+		mockPlanet.setTerrain("mountains, volcanoes, rocky deserts");
+	}
+	
+	@Test
+	public void creatingPlanetAndShouldReturn201() throws Exception{
+		URI uri = new URI("/planets");
+		mockMvc.perform(MockMvcRequestBuilders.post(uri).content(toJson(mockPlanet))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isCreated());
+	
+	}
+	
+	@Test
+	public void creatingPlanetThatAlreadyExistingAndShouldReturnBadRequest400() throws Exception{
+		URI uri = new URI("/planets");
+		mockMvc.perform(MockMvcRequestBuilders.post(uri).content(toJson(mockPlanet))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isBadRequest());
+	
+	}
+	
+	@Test
+	public void creatingEmptyPlanetAndShouldReturnBadRequest400() throws Exception{
+		URI uri = new URI("/planets");
+		mockMvc.perform(MockMvcRequestBuilders.post(uri).content("")
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isBadRequest());
+	
+	}
+	
+	
+	@Test
+	public void returnPlanetsByName() throws Exception{
+		URI uri = new URI("/planets/name/Sullust");
+		mockMvc.perform(MockMvcRequestBuilders.get(uri))
+		.andExpect(MockMvcResultMatchers.status().isOk());
+	
+	}
+	
+	@Test
+	public void returnPlanetsByWrongName() throws Exception{
+		URI uri = new URI("/planets/name/Socorro");
+		mockMvc.perform(MockMvcRequestBuilders.get(uri))
+		.andExpect(MockMvcResultMatchers.status().isNotFound());
+	
+	}
+	
+	
+	private String toJson(Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
